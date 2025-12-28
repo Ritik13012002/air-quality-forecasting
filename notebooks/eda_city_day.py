a0810ef59073ac5287da47a83a2237e401535c2a
Ritik13012002 <139044328+Ritik13012002@users.noreply.github.com>
2025-12-28 22:07:21 +0530

    Update eda_city_day.py

--- notebooks/eda_city_day.py
+++ notebooks/eda_city_day.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-# load dataset
 df = pd.read_csv("kaggle_data/city_day.csv")
 
-# basic inspection
 print("Shape:", df.shape)
 print("\nColumns:\n", df.columns)
 print("\nFirst 5 rows:\n")
